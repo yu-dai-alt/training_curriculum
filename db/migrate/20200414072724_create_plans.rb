de6dbe84cc1cd2a22d6e48e8d69cb75c658a7bb4
taroimocup <66675162+taroimocup@users.noreply.github.com>
2020-08-19 15:49:10 +0900

    Issue4

--- db/migrate/20200414072724_create_plans.rb
+++ db/migrate/20200414072724_create_plans.rb
@@ -2,7 +2,7 @@ class CreatePlans < ActiveRecord::Migration[6.0]
   def change
     create_table :plans do |t|
       t.string :plan
-      t.date :date
+      t.datetime :date
       t.timestamps
     end
   end
